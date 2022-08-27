91cad2b269a40bb0b3b129013c1df86496b88763
Himanshu <himanshu@MacBook-Pro-14-inch-2077.local>
2022-08-27 16:09:09 +0530

    end of lecture 7

---
+++ hands-on/l7/hist.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import colors
+from matplotlib.ticker import PercentFormatter
+ 
+# Creating dataset
+#np.random.seed(23685752)
+#N_points = 10000
+n_bins = 32
+mat0=np.loadtxt("num_sort.dat") 
+# Creating distribution
+ 
+# Creating histogram
+fig, ax = plt.subplots()
+#ax.hist(mat0[:,1])
+avg_wat=np.mean(mat0)
+std_wat=np.std(mat0)
+print ("Average",avg_wat)
+print ("Standard deviation", std_wat)
+ax.hist(mat0,bins=n_bins,density=True)
+ax.set_xlabel("Number of water",fontsize=12)
+ax.set_ylabel("Probability density",fontsize=12)
+
+#ax.set_ylim(ymax= 2000,ymin=-50)
+#ax.set_xlim(xmax= 180,xmin=0)
+plt.savefig('hst.png', format='png',bbox_inches='tight',dpi=300)
