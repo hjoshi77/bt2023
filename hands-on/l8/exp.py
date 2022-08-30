12a6681e9f22942bef389d07e1954a1fb31f13e7
Himanshu <himanshu@MacBook-Pro-14-inch-2077.local>
2022-08-30 16:57:44 +0530

    lecture 8

---
+++ hands-on/l8/exp.py
@@ -0,0 +1,30 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+plt.rcParams["figure.figsize"] = [6, 4]
+plt.rcParams["figure.autolayout"] = True
+
+def f(x):
+#   return np.sin(x) + x + x * np.sin(x)
+   return np.tan(x) 
+def f1(x):
+   return np.cos(x) 
+def f2(x):
+   return np.power(x,10) 
+def f3(x):
+   return np.exp(x) 
+plt.grid(color = 'green', linestyle = '--', linewidth = 0.5)
+#x = np.linspace(-10, 10, 10000)
+x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)
+plt.xlabel("x")
+plt.ylabel("F(x)")
+plt.legend(loc='upper left')
+#plt.plot(x*57.32, f2(x), color='C0',label = "x^3")
+#plt.plot(x*57.32, f1(x), color='C1',label = "cos x")
+plt.plot(x*57.32, f3(x), color='C3',label="e^x")
+ax = plt.gca()
+#ax.set_xlim([-360, 360])
+#ax.set_ylim([-1, 1])
+
+plt.legend()
+plt.savefig('test.png', dpi=100)
---
+++ hands-on/l8/funtion_normal.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+import statistics
+fig, ax = plt.subplots()  
+x_axis = np.arange(-100, 100, 1)
+  
+# Calculating mean and standard deviation
+mean = statistics.mean(x_axis)
+sd = statistics.stdev(x_axis)
+ax.set_xlabel("Distribution",fontsize=12)
+ax.set_ylabel("Probability density",fontsize=12)
+print ("mean", mean ) 
+print ("standard deviation",sd ) 
+#plt.plot(x_axis, norm.pdf(x_axis, mean, sd))
+plt.plot(x_axis, norm.pdf(x_axis, 0, 10),color='C0')
+plt.plot(x_axis, norm.pdf(x_axis, 0, 20),color="C1")
+plt.plot(x_axis, norm.pdf(x_axis, mean, sd),color='C3')
+#ax.fill_between([mean-sd,mean+sd],[0,0],[1,1],alpha=0.3,color='C0')
+#ax.fill_between([mean-2*sd,mean+2*sd],[0,0],[1,1],alpha=0.3,color='C1')
+#ax.set_ylim(ymax= 0.008,ymin=0)
+ax.set_xlim(xmax= 100,xmin=-100)
+plt.savefig('funtion_norm.png', format='png',bbox_inches='tight',dpi=300) 
